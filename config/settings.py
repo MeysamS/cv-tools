03724f439d4cb761d478c4ebeea790284b66c938
solymani-hossein <solymani_hossein@outlook.com>
2021-01-11 12:28:40 -0500

    change database sqlite to postgres

--- config/settings.py
+++ config/settings.py
@@ -105,10 +105,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cvdb',
+        'USER': 'cvuser',
+        'PASSWORD': 'cvpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
